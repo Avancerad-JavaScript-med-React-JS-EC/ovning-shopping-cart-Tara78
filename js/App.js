6ed72891723f793a88116c2e75378dc3c4dc613f
Tara78 <70331037+Tara78@users.noreply.github.com>
2020-12-18 19:52:13 +0100

    App
    App ready

---
+++ js/App.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import Header from 'Header'; 
+import Cart from 'Cart'; 
+import Product from'Product'; 
+
+function App(){
+    return(
+        <div>
+            <Header />
+            <Cart />
+            <Product />
+        </div>
+    )
+}
+
+
+
+export default App
