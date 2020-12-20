48b2efcfb1069413ff8935ead7addfd312d58e89
Tara78 <70331037+Tara78@users.noreply.github.com>
2020-12-20 21:43:42 +0100

    react

---
+++ js/app.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+import Header from './header'; 
+import Product from './product'; 
+
+
+function App() {
+    return(
+        <div>
+            <h1>Hej </h1>
+            <ul>
+
+            </ul>
+
+            <Header />
+         <Product />
+        </div>
+        
+    )
+    
+}
+
+export default App
---
+++ js/product.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+
+function Product() {
+    <div>
+        <section>
+<h1>Camila Damen </h1>
+<h3>Anna Saliva</h3>
+<p>Edith Camilla Rosins liv är präglat av hårt arbete, mörker och smärta. 
+    Hon växte upp under knapra förhållanden i femtiotalets Sundbyberg. 
+    Hemmet var  ett arbetarklasshem där hennes pappa
+     tog livet av sig i familjens badrum. </p>
+
+     <input> Put in Cart</input>
+        </section>
+    </div>
+    
+}
+
+
+export default Product;  
